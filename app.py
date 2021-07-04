6eb4e50487e79ff0ded4f0236902d13fc2b94514
Mazen Shebeta <mazenshebeta@gmail.com>
2021-07-04 14:28:03 +0200

    using sqlite and daytime

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = sqlalchemy(app)
+
+class Todo(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.string(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
